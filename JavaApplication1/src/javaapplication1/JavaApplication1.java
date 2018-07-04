89f7d56ab048afe15ced36656b7289d26ea8ae1a
AUTONOMA <AUTONOMA@L04PC16>
2018-07-04 11:25:29 -0500

    AVANCE FINAL

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,9 @@ public class JavaApplication1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        System.out.println("hola mundo!");
+         System.out.println("HOLA!");
     }
     
 }
